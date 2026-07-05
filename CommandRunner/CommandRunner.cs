48097bbf50446878a2322b822a9139a9d78e90fd
Maks5ko <maksimpatko2@gmail.com>
2026-07-05 13:06:43 +0600

    feat: add task08

---
+++ CommandRunner/CommandRunner.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using CommandLib;
+
+namespace CommandRunner
+{
+    class CommandRunner
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Использование: CommandRunner <path_to_dll>");
+                Console.WriteLine("Пример   : CommandRunner FileSystemCommands.dll");
+                return;
+            }
+
+            string dllPath = args[0];
+
+            if (!File.Exists(dllPath))
+            {
+                Console.WriteLine($"Файл '{dllPath}' не найден.");
+                return;
+            }
+
+            try
+            {
+                var assembly = Assembly.LoadFrom(dllPath);
+                Console.WriteLine($"Загружена сборка: {assembly.FullName}");
+
+                var commandTypes = assembly.GetTypes()
+                    .Where(t => t.IsClass && !t.IsAbstract && typeof(ICommand).IsAssignableFrom(t));
+
+                Console.WriteLine($"Найдено команд: {commandTypes.Count()}");
+                Console.WriteLine();
+
+                foreach (var type in commandTypes)
+                {
+                    Console.WriteLine($"Команда: {type.Name}");
+
+                    object instance = null;
+
+                    var constructors = type.GetConstructors();
+
+                    foreach (var ctor in constructors)
+                    {
+                        var parameters = ctor.GetParameters();
+                        try
+                        {
+                            var paramValues = new object[parameters.Length];
+                            for (int i = 0; i < parameters.Length; i++)
+                            {
+                                var paramType = parameters[i].ParameterType;
+                                if (paramType == typeof(string))
+                                    paramValues[i] = ".";
+                                else if (paramType == typeof(int))
+                                    paramValues[i] = 0;
+                                else if (paramType.IsValueType)
+                                    paramValues[i] = Activator.CreateInstance(paramType);
+                                else
+                                    paramValues[i] = null;
+                            }
+                            instance = ctor.Invoke(paramValues);
+                            break;
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    if (instance == null)
+                    {
+                        try
+                        {
+                            instance = Activator.CreateInstance(type);
+                        }
+                        catch
+                        {
+                            Console.WriteLine($"Не удалось создать экземпляр {type.Name} с параметрами по умолчанию");
+                            continue;
+                        }
+                    }
+
+                    if (instance is ICommand command)
+                    {
+                        Console.WriteLine("  Выполнение...");
+                        command.Execute();
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.WriteLine($"Стек вызовов: {ex.StackTrace}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileSystemCommands/FileSystemCommands.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CommandLib;
+
+namespace FileSystemCommands
+{
+    public class DirectorySizeCommand : ICommand
+    {
+        private readonly string _directoryPath;
+
+        public DirectorySizeCommand(string directoryPath)
+        {
+            _directoryPath = directoryPath ?? throw new ArgumentNullException(nameof(directoryPath));
+        }
+
+        public void Execute()
+        {
+            if (!Directory.Exists(_directoryPath))
+            {
+                Console.WriteLine($"Директория '{_directoryPath}' не найдена");
+                return;
+            }
+
+            var size = GetDirectorySize(_directoryPath);
+            Console.WriteLine($"Размер директории: {size} байт");
+        }
+
+        public long GetDirectorySize(string path)
+        {
+            var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            return files.Sum(file =>
+            {
+                try
+                {
+                    return new FileInfo(file).Length;
+                }
+                catch
+                {
+                    return 0;
+                }
+            });
+        }
+    }
+    public class FindFilesCommand : ICommand
+    {
+        private readonly string _directoryPath;
+        private readonly string _searchPattern;
+
+        public FindFilesCommand(string directoryPath, string searchPattern)
+        {
+            _directoryPath = directoryPath ?? throw new ArgumentNullException(nameof(directoryPath));
+            _searchPattern = searchPattern ?? throw new ArgumentNullException(nameof(searchPattern));
+        }
+
+        public void Execute()
+        {
+            if (!Directory.Exists(_directoryPath))
+            {
+                Console.WriteLine($"Директория '{_directoryPath}' не найдена");
+                return;
+            }
+
+            var files = Directory.GetFiles(_directoryPath, _searchPattern, SearchOption.AllDirectories);
+            Console.WriteLine($"Найдено {files.Length} файлов по маске '{_searchPattern}':");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file}");
+            }
+        }
+        public string[] GetFiles()
+        {
+            if (!Directory.Exists(_directoryPath))
+            {
+                return Array.Empty<string>();
+            }
+            return Directory.GetFiles(_directoryPath, _searchPattern, SearchOption.AllDirectories);
+        }
+    }
+}
