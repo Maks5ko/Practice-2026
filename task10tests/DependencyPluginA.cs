1df5debe52df93c178c55a8d5e4deaf7bc5f54ab
Maks5ko <maksimpatko2@gmail.com>
2026-07-09 18:52:34 +0600

    test: add task10tests

---
+++ task10tests/DependencyPluginA.cs
@@ -0,0 +1,11 @@
+﻿using PluginLib;
+
+namespace task10tests
+{
+    [PluginLoad]
+    public class DependencyPluginA : IPlugin
+    {
+        public string Name => "PluginA";
+        public void Execute() { }
+    }
+}
---
+++ task10tests/DependencyPluginB.cs
@@ -0,0 +1,12 @@
+﻿using PluginLib;
+
+namespace task10tests
+{
+    [PluginLoad]
+    [PluginDependency("PluginA")]
+    public class DependencyPluginB : IPlugin
+    {
+        public string Name => "PluginB";
+        public void Execute() { }
+    }
+}
