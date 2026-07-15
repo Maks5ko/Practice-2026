e6fd018ffdd648da0a40a74c3a288d53dff38d6b
Maks5ko <maksimpatko2@gmail.com>
2026-07-16 00:13:49 +0600

    feat: add task18

---
+++ task18/LongRunningCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace task18
+{
+    public class LongRunningCommand : ICommand
+    {
+        private readonly int _totalSteps;
+        private int _currentStep;
+        private readonly string _name;
+
+        public LongRunningCommand(string name, int steps = 5)
+        {
+            _name = name;
+            _totalSteps = steps;
+            _currentStep = 0;
+        }
+
+        public bool IsCompleted => _currentStep >= _totalSteps;
+
+        public void Execute()
+        {
+            if (IsCompleted) return;
+            _currentStep++;
+            Console.WriteLine($"Command '{_name}' step {_currentStep}/{_totalSteps} executed.");
+        }
+    }
+}
